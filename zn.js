a61a89e9dff59d077b1abb4d04a7f9b3bf13f076
sqingy <502815953@qq.com>
2020-05-19 15:51:00 +0800

    Update zn.js

--- zn.js
+++ zn.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'http://www.enzochina.com/uploads/media/index1.html\' target=\'_blank\'><img src=\'http://www.enzochina.com/uploads/media/829.jpg\' width=\'100%\'></a>");
+document.writeln("<a href=\'http://www.enzochina.com/uploads/media/index1.html\' target=\'_blank\'><img src=\'https://s1.ax1x.com/2020/05/19/Y5d6AS.jpg\' width=\'100%\'></a>");
